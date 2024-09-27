0165dbab727d454f1e453a4fb387ae51f9f053ac
dadafivethousand <maxli5004@gmail.com>
2024-09-26 20:28:00 -0400

    hi

--- src/Landing.js
+++ src/Landing.js
@@ -3,8 +3,7 @@ import leaf from './Media/leaf.png'
 export default function Landing() {
     return (
         <div className="LandingContainer">
-            <div>
-                <div className='NameAndLeaf'>
+                 <div className='NameAndLeaf'>
                 <img src={leaf}/>
             <h1> <span className='Crimson'> Maple Jiu-Jitsu <br>
             </br> Academy</span>  </h1>
@@ -13,17 +12,17 @@ export default function Landing() {
            <div className='MoveDown'>
             <h2 className='Center'>A better way to Train</h2>
             <h3 className='Center'>No Contracts | No sales tactics | Just training</h3>
-            <div>
             
-          
+            
+            
+            </div>
+            
        
           <div className="Center ButtonContainer">
               <button className="GetStartedButton">Sign Up <br></br>  </button>
            <a href='#Pricing'>   <div className="ViewPlansButton"> View Prices</div></a>
           </div>
-          </div>
-          </div>
-            </div>
+        
  
         </div>
     );
