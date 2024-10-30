7d6677cde12abe2967a931ac649b0f7f2adf7483
dadafivethousand <maxli5004@gmail.com>
2024-10-30 17:04:31 -0400

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -1,23 +1,23 @@
 const schedule = {
     Monday: [
-      { name: "BJJ All Levels", start: 7.5, end: 8.5 },
-      { name: "BJJ All Levels", start: 18.5, end: 19.5 }
+      { name: "BJJ", start: 7.5, end: 8.5 },
+      { name: "BJJ", start: 18.5, end: 19.5 }
     ],
     Tuesday: [
-      { name: "BJJ All Levels", start: 7.5, end: 8.5 },
-      { name: "BJJ All Levels", start: 18.5, end: 19.5 }
+      { name: "BJJ", start: 7.5, end: 8.5 },
+      { name: "BJJ", start: 18.5, end: 19.5 }
     ],
     Wednesday: [
-      { name: "BJJ All Levels", start: 7.5, end: 8.5 },
-      { name: "BJJ All Levels", start: 18.5, end: 19.5 }
+      { name: "BJJ", start: 7.5, end: 8.5 },
+      { name: "BJJ", start: 18.5, end: 19.5 }
     ],
     Thursday: [
-      { name: "BJJ All Levels", start: 7.5, end: 8.5 },
-      { name: "BJJ All Levels", start: 18.5, end: 19.5 }
+      { name: "BJJ", start: 7.5, end: 8.5 },
+      { name: "BJJ", start: 18.5, end: 19.5 }
     ],
     Friday: [
-      { name: "BJJ All Levels", start: 7.5, end: 8.5 },
-      { name: "BJJ All Levels", start: 18.5, end: 19.5 }
+      { name: "BJJ", start: 7.5, end: 8.5 },
+      { name: "BJJ", start: 18.5, end: 19.5 }
     ]
   };
 export default schedule  
