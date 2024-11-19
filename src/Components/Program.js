2af78acfd2a63a698ff61133bc8ba0d7c90013a1
dadafivethousand <maxli5004@gmail.com>
2024-11-19 11:01:45 -0500

    hi

--- src/Components/Program.js
+++ src/Components/Program.js
@@ -1,24 +1,18 @@
 import { useState, useEffect } from "react"
 export default function Program( {program} ){
-   const [show, setShow] = useState(false)
-   const handleClick=()=>{
-      setShow((prev)=>!prev)
-   }
+ 
+ 
     return(
         <div   className="program-container animate">
          <div className="overlay">     <h2>{program.name}</h2>
          <p>{program.description}</p> </div>
-             <div className={`${show? 'dim':''} program-image`}>
+             <div className='program-image'>
                 <img alt={program.name} src={program.image} />
              </div>
          
-             <div className={`${show? 'hiding':''} program-title`}>
-            
+             <div className='program-title'>
                 </div>
-
-           <div className={`${show? '':'hiding'} program-description`}>
-                <p>{program.description}</p>
-             </div>
+ 
              </div>
     )
 }
