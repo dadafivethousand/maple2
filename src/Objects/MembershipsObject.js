370f22a898694d91205083af475bf6b40368c983
dadafivethousand <maxli5004@gmail.com>
2024-12-02 00:01:12 -0500

    hi

--- src/Objects/MembershipsObject.js
+++ src/Objects/MembershipsObject.js
@@ -7,7 +7,7 @@
 kids:
 [
   {type: '1 Week Free Trial', price: 0,     billed: 'No commitment trial', paymentLink: 'https://buy.stripe.com/8wMbMr5jPdV9dHO004', promo: false, free: true},
-   {type: 'Monthly Membership', price: 150, cancel: 'Cancel any time', billed: 'per month', paymentLink: 'https://buy.stripe.com/8wMeYD9A57wL1Z63ch', promo: false, free: false},
+   {type: 'Monthly Membership', price: 160, cancel: 'Cancel any time', billed: 'per month', paymentLink: 'https://buy.stripe.com/8wMeYD9A57wL1Z63ch', promo: false, free: false},
   {type: 'Single Class', price: 30,  billed: 'One time payment', paymentLink: 'https://buy.stripe.com/4gw4jZ9A57wLavC6op', promo: false, free: false},
 ], 
 }
