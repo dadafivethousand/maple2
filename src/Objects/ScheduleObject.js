68e80a49667d90709d7a9983680869644d7388e3
maxim <dada@gmail.com>
2024-12-01 20:45:28 -0500

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -4,32 +4,32 @@ const schedule = {
       { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
 
       { name: "BJJ - NoGi", start: 12, end: 13 },
-      { name: "Kids - Bjj (NoGi)", start: 17.5, end: 18.33 },
+      { name: "Kids BJJ (NoGi)", start: 17.5, end: 18.33 },
 
       { name: "BJJ - NoGi", start: 18.5, end: 19.5 }
     ],
     Tuesday: [
       { name: "BJJ - Gi", start: 7.5, end: 8.5 },
       { name: "BJJ - Gi", start: 12, end: 13 },
-      { name: "Kids - Bjj (Gi)", start: 17.5, end: 18.33},
+      { name: "Kids BJJ (Gi)", start: 17.5, end: 18.33},
       { name: "BJJ - Gi", start: 18.5, end: 19.5 }
     ],
     Wednesday: [
       { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
       { name: "BJJ - NoGi", start: 12, end: 13 },
-      { name: "Kids - Wrestling", start: 17.5, end: 18.33},
+      { name: "Kids Wrestling", start: 17.5, end: 18.33},
       { name: "BJJ - NoGi", start: 18.5, end: 19.5 }
     ],
     Thursday: [
       { name: "BJJ - Gi", start: 7.5, end: 8.5 },
       { name: "BJJ - Gi", start: 12, end: 13 },
-      { name: "Kids - Bjj (Gi)", start: 17.5, end: 18.33 },
+      { name: "Kids - BJJ (Gi)", start: 17.5, end: 18.33 },
       { name: "BJJ - Gi", start: 18.5, end: 19.5 }
     ],
     Friday: [
       { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
       { name: "BJJ - NoGi", start: 12, end: 13 },
-      { name: "Kids - Wrestling", start: 17.5, end: 18.33 },
+      { name: "Kids Wrestling", start: 17.5, end: 18.33 },
       { name: "BJJ - NoGi", start: 18.5, end: 19.5 }
     ],
     Saturday: [
