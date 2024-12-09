fc6044db02f25afa1985408880965ff8b439bf00
maxim <dada@gmail.com>
2024-12-08 20:26:06 -0500

    hi

--- src/Components/GetStarted.js
+++ src/Components/GetStarted.js
@@ -7,7 +7,7 @@ export default function GetStarted({size}){
     return(
         <div className='get-started-container'>
             <div onClick={()=>setShowForm(true)} className={`${size == 'large'? 'large' : ''} get-started-button`}>
-            Start Free Trial
+           1 Week Free Trial!
           <span className='little-letters'>Beginners Welcome!</span> 
             </div>  
          
