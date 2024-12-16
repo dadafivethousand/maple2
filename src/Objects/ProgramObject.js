2b64679ff95cef390d3e97060b64dfcf621805a1
maxim <dada@gmail.com>
2024-12-15 23:32:10 -0500

    hi

--- src/Objects/ProgramObject.js
+++ src/Objects/ProgramObject.js
@@ -12,7 +12,7 @@ const Programs = [
     {image: mma,name:'Mixed Martial Arts', description: 'Our Mixed Martial Arts (MMA) program combines the best techniques from various disciplines, including striking, grappling, and ground control. With a focus on integrating techniques from Brazilian Jiu-Jitsu, Muay Thai, wrestling, and boxing, you will develop the skills needed to be a well-rounded martial artist. '},
     
     {image: wrestling,name:'Wrestling', description: 'Wrestling classes provide a dynamic environment to master the art of takedowns and control. Whether you are learning fundamental techniques, improving your transitions, or perfecting your mat skills, these sessions are designed to build strength, endurance, and technical precision.' },
-    {image: muaythai, name:'Muay Thai', description: 'Our Muay Thai program focuses on developing powerful striking techniques using punches, kicks, elbows, and knees. This dynamic training improves fitness, coordination, and self-defense skills while emphasizing discipline and mental toughness. Suitable for all levels, from beginners to advanced practitioners.'},
+    {image: muaythai, name:'Muay Thai', description: 'Our Muay Thai program focuses on developing powerful striking techniques using punches, kicks, elbows, and knees. This dynamic training improves fitness, coordination, and self-defense skills while emphasizing discipline and mental toughness. Suitable for beginners and advanced practitioners.'},
     {image: kids,name:'Kids (ages 8-12)', description: 'Our Kids program is designed to build confidence, discipline, and coordination in a fun and supportive environment. We focus on teaching the fundamentals of BJJ while instilling important life values like respect, perseverance, and teamwork.'},
 ]
 
