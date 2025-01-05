3560f0008db730f27a867e5b5086be77bad9174b
dadafivethousand <maxli5004@gmail.com>
2025-01-05 00:15:11 -0500

    hi

--- src/Components/Program.js
+++ src/Components/Program.js
@@ -6,8 +6,8 @@ export default function Program( {program} ){
         <div   className="program-container animate">
          <div className="overlay">     <h2>{program.name}</h2>
          <p>{program.description}</p>
-         {program.button?
-             <a className="book-private" href='https://maplejiujitsu.setmore.com/services/ce9112be-2765-4a91-a8c7-d8d923ac6c5a' target="_blank">
+         {program.link?
+             <a className="book-private" href={program.link} target="_blank">
                 Book Now
              </a>:null        }
           </div>
--- src/Objects/ProgramObject.js
+++ src/Objects/ProgramObject.js
@@ -15,10 +15,10 @@ const Programs = [
     
     {image: wrestling,name:'Wrestling', description: 'Wrestling classes provide a dynamic environment to master the art of takedowns and control. Whether you are learning fundamental techniques, improving your transitions, or perfecting your mat skills, these sessions are designed to build strength, endurance, and technical precision.' },
     {image: muaythai, name:'Muay Thai', description: 'Our Muay Thai program focuses on developing powerful striking techniques using punches, kicks, elbows, and knees. This dynamic training improves fitness, coordination, and self-defense skills while emphasizing discipline and mental toughness. Suitable for beginners and advanced practitioners.'},
-    {image: taj, name:'Private Training', description: 'Private training offers personalized coaching tailored to your specific goals, whether improving technique, enhancing fitness, or preparing for competition.', button: "Book Now"},
+    {image: taj, name:'Private Training', description: 'Private training offers personalized coaching tailored to your specific goals, whether improving technique, enhancing fitness, or preparing for competition.', link: 'https://maplejiujitsu.setmore.com/services/ce9112be-2765-4a91-a8c7-d8d923ac6c5a' },
 
     {image: kids,name:'Kids (ages 8-12)', description: 'Our Kids program is designed to build confidence, discipline, and coordination in a fun and supportive environment. We focus on teaching the fundamentals of BJJ while instilling important life values like respect, perseverance, and teamwork.'},
-    {image: littlekids, name:'Kids (ages 4-7)', description: 'Our Kids program for ages 4-7 is private or semi-private (max 2 per class) to ensure safety and focused learning. Classes must be reserved and are offered in English, Russian, and Ukrainian.'},
+    {image: littlekids, name:'Kids (ages 4-7)', description: 'Offered on a semi-private basis (max 2 per class) to ensure safety and focused learning. These sessions are require a reservation.', link: 'https://maplejiujitsu.setmore.com/classes/eb24a3de-f7f8-4392-8da5-1818e902d662'},
 
 ]
 
