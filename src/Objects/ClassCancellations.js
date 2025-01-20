459412dcb4d8e09742cb2248799f63ef3a4d341c
maxim <dada@gmail.com>
2025-01-20 12:01:45 -0500

    hi

--- src/Objects/ClassCancellations.js
+++ src/Objects/ClassCancellations.js
@@ -1,10 +1,10 @@
 const cancellations = [
-    {day: 'January 22', name: "BJJ NoGi", time: "7:30 am - 8:30 am"},
-    {day: 'January 22', name: "BJJ NoGi", time: "12:00 pm - 1:00 pm"},
-    {day: 'January 23', name: "BJJ Gi", time: "7:30 am - 8:30 am"},
-    {day: 'January 23', name: "BJJ Gi", time: "12:00 pm - 1:00 pm"},
-    {day: 'January 24', name: "BJJ NoGi", time: "7:30 am - 8:30 am"},
-    {day: 'January 24', name: "BJJ NoGi", time: "12:00 pm - 1:00 pm"},
+    {day: 'January 22', name: "BJJ - NoGi", time: "7:30 am - 8:30 am"},
+    {day: 'January 22', name: "BJJ - NoGi", time: "12:00 pm - 1:00 pm"},
+    {day: 'January 23', name: "BJJ - Gi", time: "7:30 am - 8:30 am"},
+    {day: 'January 23', name: "BJJ - Gi", time: "12:00 pm - 1:00 pm"},
+    {day: 'January 24', name: "BJJ - NoGi", time: "7:30 am - 8:30 am"},
+    {day: 'January 24', name: "BJJ - NoGi", time: "12:00 pm - 1:00 pm"},
  
   ];
   export default cancellations  
