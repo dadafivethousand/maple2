9bf89571e2f25921c823798b1a329c5f4d446923
maxim <dada@gmail.com>
2025-01-31 21:47:53 -0500

    hi

--- src/Objects/ProgramObject.js
+++ src/Objects/ProgramObject.js
@@ -7,6 +7,7 @@ import muaythai from '../Media/mt.png'
 import taj from '../Media/Taj.jpg'
 import littlekids from '../Media/littlekids.jpg'
 import mijain from '../Media/mijain.jpg'
+import freestyle from '../Media/freestyle.jpeg'
 
 import wrestling from '../Media/wrestling.jpeg'
 const Programs = [
@@ -15,7 +16,7 @@ const Programs = [
     {image: mma,name:'Mixed Martial Arts (MMA)', description: 'Our Mixed Martial Arts (MMA) program combines the best techniques from various disciplines, including striking, grappling, and ground control. With a focus on integrating techniques from Brazilian Jiu-Jitsu, Muay Thai, wrestling, and boxing, you will develop the skills needed to be a well-rounded martial artist. '},
     {image: mijain ,name:'Greco-Roman Wrestling', description: 'Greco-Roman Wrestling focuses on upper-body control, throws, and dominant positioning. This class emphasizes mastering clinch techniques, powerful takedowns, and relentless mat pressure. Whether you are a beginner or an experienced grappler, these sessions develop strength, endurance, and technical precision to elevate your wrestling game.' },
 
-    {image: wrestling,name:'Freestyle Wrestling', description: 'Freestyle Wrestling is a fast-paced and dynamic grappling style that allows attacks on both the upper and lower body. This class focuses on explosive takedowns, leg attacks, and fluid transitions to control and pin your opponent. Whether you are building a strong foundation or sharpening advanced techniques, these sessions develop speed, strength, and tactical awareness to dominate on the mat.' },
+    {image: freestyle,name:'Freestyle Wrestling', description: 'Freestyle Wrestling is a fast-paced and dynamic grappling style that allows attacks on both the upper and lower body. This class focuses on explosive takedowns, leg attacks, and fluid transitions to control and pin your opponent. Whether you are building a strong foundation or sharpening advanced techniques, these sessions develop speed, strength, and tactical awareness to dominate on the mat.' },
     {image: muaythai, name:'Muay Thai', description: 'Our Muay Thai program focuses on developing powerful striking techniques using punches, kicks, elbows, and knees. This dynamic training improves fitness, coordination, and self-defense skills while emphasizing discipline and mental toughness. Suitable for beginners and advanced practitioners.'},
     {image: taj, name:'Private Training', description: 'Private training offers personalized coaching tailored to your specific goals, whether improving technique, enhancing fitness, or preparing for competition.', link: 'https://maplejiujitsu.setmore.com/services/ce9112be-2765-4a91-a8c7-d8d923ac6c5a' },
 
