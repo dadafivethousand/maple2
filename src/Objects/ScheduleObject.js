c1ac422204ac2368a67f77b118e3721fe56d5cde
maxim <dada@gmail.com>
2025-06-08 17:09:01 -0400

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -2,14 +2,14 @@ const schedule = {
   Monday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
- 
+        { name: "MMA", start: 17.5, end: 18.5 },
     { name: "Wrestling", start: 18.5, end: 19.5 },
     { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
   ],
   Tuesday: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-        { name: "Kids", start: 17.5, end: 18.5},
+    { name: "Kids", start: 17.5, end: 18.5},
     { name: "Muay Thai", start: 18.5, end: 19.5 },
     { name: "BJJ - Gi", start: 19.5, end: 20.5 }
   ],
@@ -31,18 +31,18 @@ const schedule = {
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
  
+        { name: "MMA", start: 17.5, end: 18.5 },
     { name: "Wrestling", start: 18.5, end: 19.5 },
     { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
   ],
   Saturday: [ 
  
-    { name: "BJJ - NoGi", start: 12, end: 13},
+    { name: "BJJ - Gi", start: 12, end: 13},
       { name: "Kids", start: 14, end: 15},
   ]
   ,
   Sunday: [
     { name: "Kids", start: 11, end: 12},
- 
     { name: "Open Mat", start: 12, end: 13
       
     },
