102c001d78b3d3de2efb7d03f9f4db8c829f1ce5
arturo12311 <liartur19@gmail.com>
2025-12-07 12:25:58 -0500

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -2,50 +2,44 @@ const schedule = {
   Monday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
-        { name: "MMA", start: 17.5, end: 18.5 },
-    { name: "Wrestling", start: 18.5, end: 19.5 },
-    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
+        { name: "Kids (5-12)", start: 17, end: 18 },
+        { name: "MMA", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20 },
+    { name: "BJJ - NoGi", start: 20, end: 21 }
   ],
   Tuesday: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-    { name: "Kids", start: 17.5, end: 18.5},
-    { name: "Muay Thai", start: 18.5, end: 19.5 },
-    { name: "BJJ - Gi", start: 19.5, end: 20.5 }
+      { name: "Kids (5-12)", start: 17, end: 18 },
+
+    { name: "Muay Thai", start: 18, end: 19 },
+        { name: "Wrestling", start: 19, end: 20 },
+    { name: "BJJ - Gi", start: 20, end: 21 }
   ],
   Wednesday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
-    { name: "MMA", start: 17.5, end: 18.5 },
-    { name: "Wrestling", start: 18.5, end: 19.5 },
-    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
+    { name: "Kids (5-12)", start: 17, end: 18 },
+    { name: "MMA", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20 },
+    { name: "BJJ - NoGi", start: 20, end: 21 }
   ],
-  Thursday: [
+ ThursdaY: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-        { name: "Kids", start: 17.5, end: 18.5},
-    { name: "Muay Thai", start: 18.5, end: 19.5},
-    { name: "BJJ - Gi", start: 19.5, end: 20.5 }
+    { name: "Kids (5-12)", start: 17, end: 18 },
+    { name: "Muay Thai", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20 },
+    { name: "BJJ - Gi", start: 20, end: 21 }
   ],
   Friday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
- 
-        { name: "MMA", start: 17.5, end: 18.5 },
-    { name: "Wrestling", start: 18.5, end: 19.5 },
-    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
+    { name: "MMA", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20 },
+    { name: "BJJ - NoGi", start: 20, end: 21 }
   ],
-  Saturday: [ 
  
-    { name: "BJJ - Gi", start: 12, end: 13},
-      { name: "Kids", start: 14, end: 15},
-  ]
-  ,
-  Sunday: [
-    { name: "Kids", start: 11, end: 12},
-    { name: "Open Mat", start: 12, end: 13
-      
-    },
-  ]
+ 
 };
 export default schedule  
