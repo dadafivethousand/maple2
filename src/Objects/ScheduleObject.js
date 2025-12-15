c4195b756d9b00e5d878abdf75b7f37b90343fb6
maxim <dada@gmail.com>
2025-12-14 21:00:44 -0500

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -3,38 +3,38 @@
   Monday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
-        { name: "MMA", start: 18, end: 19 },
-    { name: "Wrestling", start: 19, end: 20},
-    { name: "BJJ - NoGi", start: 20, end: 21 }
+        { name: "MMA", start: 17.5, end: 18.5 },
+    { name: "Wrestling", start: 18.5, end: 19.5},
+    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
   ],
   Tuesday: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-    { name: "Kids", start: 18, end: 19},
-    { name: "Muay Thai", start: 19, end: 20},
-    { name: "BJJ - Gi", start: 20, end: 21 }
+    { name: "Kids", start: 17.5, end: 18.5},
+    { name: "Muay Thai", start: 18.5, end: 19.5},
+    { name: "BJJ - Gi", start: 19.5, end: 20.5 }
   ],
   Wednesday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
-    { name: "MMA", start: 18, end: 19 },
-    { name: "Wrestling", start: 19, end: 20},
-    { name: "BJJ - NoGi", start: 20, end: 21 }
+    { name: "MMA", start: 17.5, end: 18.5 },
+    { name: "Wrestling", start: 18.5, end: 19.5},
+    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
   ],
   Thursday: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-        { name: "Kids", start: 18, end: 19},
-    { name: "Muay Thai", start: 19, end: 20},
-    { name: "BJJ - Gi", start: 20, end: 21 }
+        { name: "Kids", start: 17.5, end: 18.5},
+    { name: "Muay Thai", start: 18.5, end: 19.5},
+    { name: "BJJ - Gi", start: 19.5, end: 20.5 }
   ],
   Friday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
  
-        { name: "MMA", start: 18, end: 19 },
-    { name: "Wrestling", start: 19, end: 20},
-    { name: "BJJ - NoGi", start: 20, end: 21 }
+        { name: "MMA", start: 17.5, end: 18.5 },
+    { name: "Wrestling", start: 18.5, end: 19.5},
+    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
   ],
   Saturday: [ 
  
