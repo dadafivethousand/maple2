78dc9644f68e4be1e71ec31ca1edaaa5fed430e5
maxim <dada@gmail.com>
2026-01-04 13:32:54 -0500

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -3,51 +3,51 @@
   Monday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
-        { name: "MMA", start: 17.5, end: 18.5 },
-    { name: "Wrestling", start: 18.5, end: 19.5},
-    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
+    { name: "Kids", start: 17, end: 18 },
+    { name: "MMA", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20},
+    { name: "BJJ - NoGi", start: 20, end: 21 }
   ],
   Tuesday: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-    { name: "Kids", start: 17.5, end: 18.5},
-    { name: "Muay Thai", start: 18.5, end: 19.5},
-    { name: "BJJ - Gi", start: 19.5, end: 20.5 }
-  ],
+    { name: "Kids", start: 17, end: 18},
+    { name: "Muay Thai", start: 18, end: 19},
+        { name: "Wrestling", start: 19, end: 20},
+    { name: "BJJ - Gi", start: 20, end: 21 }  ],
+    
   Wednesday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
-    { name: "MMA", start: 17.5, end: 18.5 },
-    { name: "Wrestling", start: 18.5, end: 19.5},
-    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
+    { name: "Kids", start: 17, end: 18 },
+    { name: "MMA", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20},
+    { name: "BJJ - NoGi", start: 20, end: 21 }
   ],
   Thursday: [
     { name: "BJJ - Gi", start: 7.5, end: 8.5 },
     { name: "BJJ - Gi", start: 12.5, end: 13.5 },
-        { name: "Kids", start: 17.5, end: 18.5},
-    { name: "Muay Thai", start: 18.5, end: 19.5},
-    { name: "BJJ - Gi", start: 19.5, end: 20.5 }
+    { name: "Kids", start: 17, end: 18 },
+    { name: "Muay Thai", start: 18, end: 19},
+        { name: "Wrestling", start: 19, end: 20},
+    { name: "BJJ - Gi", start: 20, end: 21 }
   ],
   Friday: [
     { name: "BJJ - NoGi", start: 7.5, end: 8.5 },
     { name: "BJJ - NoGi", start: 12.5, end: 13.5 },
- 
-        { name: "MMA", start: 17.5, end: 18.5 },
-    { name: "Wrestling", start: 18.5, end: 19.5},
-    { name: "BJJ - NoGi", start: 19.5, end: 20.5 }
+
+    { name: "MMA", start: 18, end: 19 },
+    { name: "Wrestling", start: 19, end: 20},
+    { name: "BJJ - NoGi", start: 20, end: 21 }
   ],
   Saturday: [ 
  
-    { name: "BJJ - Gi", start: 12, end: 13},
-      { name: "Kids", start: 14, end: 15},
+    { name: "Muay-Thai", start: 12, end: 13},
+    { name: "Open Mat", start: 13, end: 14}
   ]
-  ,
-  Sunday: [
-    { name: "Kids", start: 11, end: 12},
-    { name: "Open Mat", start: 12, end: 13
-      
-    },
-  ]
+  
+ 
+ 
 };
 
 
--- src/Schedule.js
+++ src/Schedule.js
@@ -110,7 +110,7 @@ export default function Schedule() {
   return (
     <div id="Schedule" className='ScheduleContainer'>
  <h1 className='animate'> 🗓️ Schedule</h1>
-    <ScheduleWidget />  
+ 
       <div className="Schedule">
         {Object.keys(schedule).map((day) => (
           <div className='Column animate' key={day}>
