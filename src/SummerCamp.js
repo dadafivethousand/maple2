ca3a32cb0fb9ec4442e851038067023bf2d9e57c
arturo12311 <liartur19@gmail.com>
2026-04-04 22:00:42 -0400

    hi

--- src/SummerCamp.js
+++ src/SummerCamp.js
@@ -104,15 +104,7 @@ export default function SummerCamp() {
 
             <button className="sc-hero-cta" onClick={scrollToPicker}>
               Register Now
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-                <path
-                  d="M12 5v14M5 12l7 7 7-7"
-                  stroke="currentColor"
-                  strokeWidth="2.2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+       
             </button>
           </div>
 
